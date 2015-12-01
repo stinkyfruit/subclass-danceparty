edab36f0132cdf4caa8bffd3fee84dbed41ddd7f
Kristen Haydel <kristen@whatever.com>
2015-12-01 13:14:56 -0600

    Added new dancer types

---
+++ src/squareDancer.js
@@ -0,0 +1,18 @@
+var squareDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);  
+
+} 
+
+squareDancer.prototype = Object.create(Dancer.prototype);
+squareDancer.prototype.constructor = squareDancer;
+
+
+squareDancer.prototype.step = function(){
+  Dancer.prototype.step.call(this);
+  //make it do this every step slide down
+  this.$node.animate({height: '300px', opacity: '0.4'}, "slow");
+  this.$node.animate({width: '300px', opacity: '0.8'}, "slow");
+  this.$node.animate({height: '100px', opacity: '0.4'}, "slow");
+  this.$node.animate({width: '100px', opacity: '0.8'}, "slow");
+ 
+}
--- src/taylorDancer.js
+++ src/taylorDancer.js
@@ -1,7 +1,8 @@
 var TaylorDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<img src="src/Taylor123.png" height="100" width="100">')
+  var taylor = $('<img src="src/Taylor123.png" height="100" width="100">')
 
+  this.$node.append(taylor);
 }
 
 TaylorDancer.prototype = Object.create(Dancer.prototype);
